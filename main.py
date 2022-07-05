003c1aadbba98e6bf75ebd2bab37c271746a963c
5rq <103810168+5rq@users.noreply.github.com>
2022-07-05 03:00:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ from keepalive import keep_alive
 
 token = os.getenv("token")
 prefix = os.getenv("prefix")
+
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+
+@bot.event
+async def on_ready():
+    print(f'Running as {bot.user.name}#{bot.user.discriminator}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency * 1000)}ms')
+
+bot.run(token)
